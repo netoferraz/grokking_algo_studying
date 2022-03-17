2eb768d2c722ae5b84e0c7296850abcf1670b8eb
netoferraz <netoferraz@gmail.com>
2022-03-17 16:26:03 -0300

    recursive_max_value

--- ch04/divide_and_conquer.py
+++ ch04/divide_and_conquer.py
@@ -1,5 +1,7 @@
 def recursive_sum(array):
-    """write a functions that recieve a list and output the sum of the elements."""
+    """write a functions that recieve a list and output the sum of the elements.
+    exercise 4.1
+    """
     #define a base case
     if len(array) == 1:
         return array[0]
@@ -7,9 +9,22 @@ def recursive_sum(array):
         return array[0] + recursive_sum(array[1:])    
 
 def recursive_count_items(array):
-    """write a function that counts elements of a list in a recursive way."""
+    """write a function that counts elements of a list in a recursive way.
+    exercise 4.2
+    """
     # define a base case
     if len(array) == 1:
         return 1
     else: #recurive count
         return 1 + recursive_count_items(array[1:])
+
+def recursive_max_value(array):
+    """write a function that finds the greatest value in a list in a recursive way
+    exercise 4.3
+    """
+    # define a base case
+    if len(array) == 1:
+        return array[0]
+    else:
+        max_value = recursive_max_value(array[1:])
+        return max_value if max_value > array[0] else array[0]
--- tests/test_divide_and_conquer.py
+++ tests/test_divide_and_conquer.py
@@ -1,4 +1,4 @@
-from ch04.divide_and_conquer import recursive_sum, recursive_count_items
+from ch04.divide_and_conquer import recursive_sum, recursive_count_items, recursive_max_value
 import pytest
 @pytest.mark.parametrize('expected, test_input', [(12, [2,4,6]), (5, [5]), (10, [8,2]), (20, [10,5,3,2])])
 def test_recursive_sum(expected, test_input):
@@ -7,3 +7,7 @@ def test_recursive_sum(expected, test_input):
 @pytest.mark.parametrize('expected, test_input', [(3, [2,4,6]), (1, [5]), (2, [8,2]), (4, [10,5,3,2])])
 def test_recursive_count_items(expected, test_input):
     assert expected == recursive_count_items(test_input)
+
+@pytest.mark.parametrize('expected, test_input', [(5, [1,2,3,4,5]), (5, [5]), (10, [8,2,10]), (20, [10,5,3,2,20])])
+def test_recursive_max_value(expected, test_input):
+    assert expected == recursive_max_value(test_input)
