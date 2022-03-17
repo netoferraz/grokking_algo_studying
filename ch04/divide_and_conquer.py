4a165d116871f6bf552e9ec1d3583bff608cdd81
netoferraz <netoferraz@gmail.com>
2022-03-16 22:39:55 -0300

    recursive_count_items tests

--- ch04/divide_and_conquer.py
+++ ch04/divide_and_conquer.py
@@ -5,3 +5,11 @@ def recursive_sum(array):
         return array[0]
     else: #recursion case
         return array[0] + recursive_sum(array[1:])    
+
+def recursive_count_items(array):
+    """write a function that counts elements of a list in a recursive way."""
+    # define a base case
+    if len(array) == 1:
+        return 1
+    else: #recurive count
+        return 1 + recursive_count_items(array[1:])
--- tests/test_divide_and_conquer.py
+++ tests/test_divide_and_conquer.py
@@ -1,5 +1,9 @@
-from ch04.divide_and_conquer import recursive_sum
+from ch04.divide_and_conquer import recursive_sum, recursive_count_items
 import pytest
 @pytest.mark.parametrize('expected, test_input', [(12, [2,4,6]), (5, [5]), (10, [8,2]), (20, [10,5,3,2])])
 def test_recursive_sum(expected, test_input):
     assert expected == recursive_sum(test_input)
+
+@pytest.mark.parametrize('expected, test_input', [(3, [2,4,6]), (1, [5]), (2, [8,2]), (4, [10,5,3,2])])
+def test_recursive_count_items(expected, test_input):
+    assert expected == recursive_count_items(test_input)
